048f9c9f839c84beed2e8ff7a14ffff53091f473
migs <programming@maggiesullivan.us>
2023-09-17 23:30:39 -0700

    updated eventtime proceesing time

--- src/main/scala/modernflink/section2/EventTimeProcessingTime.scala
+++ src/main/scala/modernflink/section2/EventTimeProcessingTime.scala
@@ -1,7 +1,6 @@
-package scalabackup.section2
+package modernflink.section2
 
-import scalabackup.modelbackup.{SubscriptionEvent, SubscriptionEventsGenerator}
-import scalabackup.section2.Given.instantTypeInfo
+import modernflink.model.{SubscriptionEventsGenerator, SubscriptionEvent}
 import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.windowing.assigners.{TumblingEventTimeWindows, TumblingProcessingTimeWindows}
@@ -9,38 +8,34 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.util.Collector
 import org.apache.flinkx.api.serializers.*
 import org.apache.flinkx.api.StreamExecutionEnvironment
-
 import java.time.{Duration, Instant}
+import modernflink.model.instantTypeInfo
 
 val env = StreamExecutionEnvironment.getExecutionEnvironment
 
 val subscriptionEvent = env.addSource(
   new SubscriptionEventsGenerator(
-    sleepSeconds = 1,
-    startTime = Instant.parse("2023-08-13T00:00:00.00Z")
-  )
-)
+    sleepSeconds = 1, startTime = Instant.parse("2023-08-13T00:00:00.00Z")
+  ))
 
 // Processing Time
 def processingTimeDemo(): Unit =
-
   val eventStream1 = subscriptionEvent
     .keyBy(_.userId)
     .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
     .apply(
-      (
-          userId,
-          timeWindow,
-          events,
-          collector: Collector[(String, String)]
+      (userId,
+       timeWindow,
+       events,
+       collector:Collector[(String, String)]
       ) => {
         collector.collect(
           userId,
           events
-            .map(e =>
-              s"Processing Time Window ${timeWindow.getStart}-${timeWindow.getEnd}: ${e.getClass.getSimpleName} at ${e.time}"
-            )
-            .mkString(", ")
+          .map(e =>
+            s"Processing Time Window ${timeWindow.getStart} - ${timeWindow.getEnd}: ${e.getClass.getSimpleName} at ${e.time}"
+          )
+          .mkString(",")
         )
       }
     )
@@ -54,15 +49,9 @@ def eventTimeDemo(): Unit =
     .assignTimestampsAndWatermarks(
       WatermarkStrategy
         .forBoundedOutOfOrderness(Duration.ofSeconds(10))
-        .withTimestampAssigner(
-          new SerializableTimestampAssigner[SubscriptionEvent] {
-            override def extractTimestamp(
-                element: SubscriptionEvent,
-                recordTimestamp: Long
-            ) =
-              element.time.toEpochMilli
-          }
-        )
+        .withTimestampAssigner(new SerializableTimestampAssigner[SubscriptionEvent]:
+            override def extractTimestamp(element: SubscriptionEvent, recordTimestamp: Long): Long = element.time.toEpochMilli
+          )
     )
 
   val userActionStream2 = withWatermarks
@@ -70,19 +59,17 @@ def eventTimeDemo(): Unit =
     .window(TumblingEventTimeWindows.of(Time.seconds(5)))
     .apply(
       (
-          userId,
-          timeWindow,
-          events,
-          collector: Collector[(String, String)]
-      ) => {
+        userId,
+        timeWindow,
+        events,
+        collector:Collector[(String, String)]
+        ) => {
         collector.collect(
-          (
-            userId,
+          (userId,
             events
               .map(e =>
-                s"Event Time Window ${timeWindow.getStart}-${timeWindow.getEnd}: ${e.getClass.getSimpleName}: ${e.time}"
-              )
-              .mkString(", ")
+                s"Event Time Window ${timeWindow.getStart} - ${timeWindow.getEnd}: ${e.getClass.getSimpleName}: ${e.time}"
+              ).mkString
           )
         )
       }
@@ -91,6 +78,6 @@ def eventTimeDemo(): Unit =
   userActionStream2.print()
   env.execute()
 
-@main def eventTimeProcessingTime() =
-//    processingTimeDemo()
-    eventTimeDemo()
+@main def eventTimeProcessingTimeDemo() =
+//  processingTimeDemo()
+  eventTimeDemo()
--- src/main/scala/modernflink/section2/GlobalWindow.scala
+++ src/main/scala/modernflink/section2/GlobalWindow.scala
@@ -1,6 +1,6 @@
 package modernflink.section2
 
-import scalabackup.modelbackup.*
+import modernflink.model.{AboveAverage, Average, BelowAverage, HumidityLevel, HumidityReading}
 import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows
 import org.apache.flink.streaming.api.windowing.triggers.{CountTrigger, PurgingTrigger}
 import org.apache.flink.streaming.api.windowing.windows.{GlobalWindow, Window}
@@ -10,6 +10,39 @@ import org.apache.flinkx.api.serializers.*
 import org.apache.flinkx.api.StreamExecutionEnvironment
 import java.time.{Duration, Instant}
 
+class GlobalWindowDemo extends WindowFunction[HumidityReading, String, String, GlobalWindow]:
+  override def apply(
+                      key: String,
+                      window: GlobalWindow,
+                      input: Iterable[HumidityReading],
+                      out: Collector[String]
+                    ): Unit =
+    val averageGlobalWindow = input.map(_.humidity).sum / input.size
+    val last = input.last
+
+    val humidityLevel: HumidityLevel =
+      if last.humidity > averageGlobalWindow then AboveAverage
+      else if last.humidity == averageGlobalWindow then Average
+      else BelowAverage
+    out.collect(
+        s"$key - ${input.map(_.timestamp)} - $averageGlobalWindow - $last - $humidityLevel"
+      )
+
 @main def globalWindowDemo() =
   val env = StreamExecutionEnvironment.getExecutionEnvironment
-  
+
+  val inputFile = env.readTextFile("src/main/resources/Humidity.txt")
+  val humidityData = inputFile.map(HumidityReading.fromString)
+
+  val outputGlobalWindowStream = humidityData
+    .keyBy(_.location)
+    .window(GlobalWindows.create())
+    .trigger(
+      PurgingTrigger.of(CountTrigger.of[Window](5))
+    )
+    .apply(new GlobalWindowDemo)
+
+  outputGlobalWindowStream.print()
+  env.execute()
+
+
