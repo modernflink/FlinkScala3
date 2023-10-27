b7d388db9aab6eded560927778de736378c962d0
migs <programming@maggiesullivan.us>
2023-10-26 19:25:32 -0700

    multiple streams remove environment

--- src/main/scala/modernflink/section1/MultipleStreams.scala
+++ src/main/scala/modernflink/section1/MultipleStreams.scala
@@ -3,5 +3,3 @@ package modernflink.section1
 import org.apache.flinkx.api.serializers.*
 import org.apache.flinkx.api.{ConnectedStreams, DataStream, KeyedStream, StreamExecutionEnvironment}
 import modernflink.model.{HumidityReading, LocalSummary, TemperatureReading}
-
-val env = StreamExecutionEnvironment.getExecutionEnvironment
